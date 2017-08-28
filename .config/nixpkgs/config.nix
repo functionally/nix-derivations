23dd26f7461c1ae07c76eddde193a6df22e488dc
Brian W Bush <consult@brianwbush.info>
2017-08-27 20:59:40 -0600

    revised system and user configuration

---
+++ .config/nixpkgs/config.nix
@@ -0,0 +1,63 @@
+{
+
+  allowUnfree = true;
+
+# chromium = {
+#   enablePepperFlash = true;
+#   enablePepperPDF = true;
+# };
+
+  packageOverrides = super:
+    let
+      self = super.pkgs;
+      unstable = import <nixos-unstable>{};
+    in
+      with self; rec {
+ 
+        lemur = with pkgs; buildEnv {
+          name = "lemur";
+          paths =
+            [
+              # Command-line
+              ffmpeg
+              imagemagick
+              librdf_raptor2
+              librdf_rasqal
+              # Graphical
+              blender
+              gephi
+              ggobi
+              graphviz
+              guvcview
+              inkscape
+              libreoffice
+              meshlab
+              pandoc
+              paraview
+              qgis
+              rstudio
+              scid
+#             (import <nixos-unstable> {}).google-chrome
+              unstable.google-chrome
+              # Communication
+              discord
+              gajim
+              skype
+              slack
+              tigervnc
+              # Xfce
+              xfce.mousepad
+              xfce.parole
+              # Programming
+              jre
+              python3
+              R
+              # Services
+              awscli
+              google-cloud-sdk
+            ];
+        };
+
+    };
+
+}
