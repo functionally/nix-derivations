2f01fc724db05a9788a7f28217ccdb7710d549d6
Brian W Bush <consult@brianwbush.info>
2017-08-31 23:19:08 -0600

    revised system and user configuration

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -2,11 +2,6 @@
 
   allowUnfree = true;
 
-# chromium = {
-#   enablePepperFlash = true;
-#   enablePepperPDF = true;
-# };
-
   packageOverrides = super:
     let
       self = super.pkgs;
@@ -16,58 +11,88 @@
  
         lemur = with pkgs; buildEnv {
           name = "lemur";
-          paths =
-            [
-              # Command-line
-              ffmpeg
-              imagemagick
-              librdf_raptor2
-              librdf_rasqal
-              # Graphical
-              blender
-              gephi
-              ggobi
-              graphviz
-              guvcview
-              inkscape
-              libreoffice
-              meshlab
-              pandoc
-              paraview
-              qgis
-              rstudio
-              scid
-#             (import <nixos-unstable> {}).google-chrome
-              unstable.google-chrome
-              # Communication
-#             discord
-              gajim
-              skype
-              slack
-              tigervnc
-              # Xfce
-              xfce.mousepad
-              xfce.parole
-              # Programming
-              jre
-              python3
-              R
-              # Services
-              awscli
-              google-cloud-sdk
-            ];
+          paths = [
+            # Command-line
+            ffmpeg
+            imagemagick
+            librdf_raptor2
+            librdf_rasqal
+            # Graphical
+            blender
+            gephi
+            ggobi
+            graphviz
+            guvcview
+            inkscape
+            libreoffice
+            meshlab
+            pandoc
+            paraview
+            qgis
+            rstudio
+            scid
+#           (import <nixos-unstable> {}).google-chrome
+            unstable.google-chrome
+            # Communication
+#           discord
+            gajim
+            skype
+            slack
+            tigervnc
+            # Xfce
+            xfce.mousepad
+            xfce.parole
+            # Programming
+            jre
+            python3
+            R
+            # Services
+            awscli
+            google-cloud-sdk
+          ];
         };
 
-        lemurHaskell = self.haskell.packages.ghc7103.ghcWithPackages
-          (
-            haskellPackages: with haskellPackages;
-            [
-              # libraries
-              arrows async cgi criterion
-              # tools
-              cabal-install haskintex
-            ]
-          );
+        myHaskellPackages = libProf: self: super:
+          with pkgs.haskell.lib; let pkg = self.callPackage; in rec {
+
+            raft = pkg /scratch/raft {};
+            daft = pkg /scratch/daft {};
+
+          # ghcmod7 = pkg ./ghc-mod-4.1.6.nix {};
+
+            mkDerivation = args: super.mkDerivation (args // {
+              enableLibraryProfiling = libProf;
+              enableExecutableProfiling = false;
+            });
+
+          };
+
+        haskell7Packages = super.haskell.packages.ghc7103.override {
+          overrides = myHaskellPackages false;
+        };
+
+        ghcEnv = pkgs.myEnvFun {
+          name = "ghc7";
+          buildInputs = with haskell7Packages; [
+            (ghcWithHoogle (h: [ ]))
+            cabal-install
+          # ghcmod7
+            ghcid
+            hdevtools
+            hlint
+            pointfree
+            pointful
+          # threadscope
+          ];
+        };
+
+        haskellEnv = pkgs.buildEnv {
+          name = "env-haskell";
+          paths = [
+            nix-prefetch-git
+            cabal2nix
+          ];
+        };
 
       };
 
