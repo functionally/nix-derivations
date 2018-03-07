ddabca09e4b9f98c67e3e02369977f3068e58104
Brian W Bush <consult@brianwbush.info>
2018-03-06 18:57:37 -0700

    added R environment

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -276,6 +276,7 @@
             git
             gnumake
             gnupg
+            inotify-tools
             mercurial
             mkpasswd
             nix-repl
@@ -538,6 +539,78 @@
             ]))
           ];
         };
+
+        rEnv = let
+                 deriveR = {name, src, buildInputs, postPatch}:
+                   stdenv.mkDerivation {
+                     inherit name;
+                     inherit src;
+                     inherit postPatch;
+                     buildInputs = [R] ++ buildInputs;
+                     configurePhase = ''
+                       runHook preConfigure
+                       export R_LIBS_SITE="$R_LIBS_SITE''${R_LIBS_SITE:+:}$out/library"
+                       runHook postConfigure
+                     '';
+                     buildPhase = ''
+                       runHook preBuild
+                       runHook postBuild
+                     '';
+                     installFlags = [];
+                     rCommand = "R";
+                     installPhase = ''
+                       runHook preInstall
+                       mkdir -p $out/library
+                       $rCommand CMD INSTALL $installFlags --configure-args="$configureFlags" -l $out/library .
+                       runHook postInstall
+                     '';
+                     postFixup = ''
+                       if test -e $out/nix-support/propagated-native-build-inputs; then
+                           ln -s $out/nix-support/propagated-native-build-inputs $out/nix-support/propagated-user-env-packages
+                       fi
+                     '';
+                     checkPhase = ''
+                       # noop since R CMD INSTALL tests packages
+                     '';
+                 };
+                 iplot =
+                   let
+                     aname = "iplot";
+                     version = "1.1.0";
+                   in
+                     deriveR {
+                       name = "r-${aname}-${version}";
+                       src = fetchgit {
+                         url = "https://github.nrel.gov/InsightCenter/iplot.git";
+                         rev = "f9d6090bceabc83bda520452c8b0098610ad3d64";
+                         sha256="0ynrm3v68b2f20s8kc4x23hcxfnyzhzpn6ig9x47zs90yn5zdn9n";
+                       };
+                       postPatch = ''
+                       '';
+                       buildInputs = [
+                         openssl
+                         zlib
+                         rPackages.Rcpp
+                         rPackages.jsonlite
+                       ];
+                     };
+               in
+                 pkgs.buildEnv {
+                   name = "env-r";
+                   paths = with rPackages; [
+#                super.rstudioWrapper.override {
+#                  packages = with self.rPackages; [
+                     R
+                   # iplot
+                     codetools
+                     data_table
+                     ggplot2
+                     jsonlite
+                     plotrix
+                     Rcpp
+                     shiny
+                   ];
+                 };
 
       } // (
 
