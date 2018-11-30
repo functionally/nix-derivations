6164c1fae0db6e508eae3c08b06ba1c2931a8891
Brian W Bush <consult@brianwbush.info>
2018-11-30 15:52:06 -0700

    NIXOS-60 moved unity environment to separate file

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -352,40 +352,7 @@
 
         inherit (import ./haskell.nix { inherit pkgs pin1709 pin1803 unstable; }) haskellEnv ghcEnv7 ghcEnv8 ghcEnvLatest;
 
-        unity3d = super.stdenv.lib.overrideDerivation super.unity3d (attrs: {
-          preFixup = ''
-            find $unitydir -name PlaybackEngines -prune -o \( -type f -name \*.so -not -name libunity-nosuid.so \) -print | while read path
-            do
-              oldrpath=$(patchelf --print-rpath "$path")
-              # TODO: Combine the three sed scripts into one.
-              newrpath=$(echo $oldrpath | sed -e "s/:/\n/g" | sed -e "/^\/usr/d ; /^\/home/d ; /^\/tmp/d" | sed -e :a -e "/$/N; s/\n/:/; ta")
-              if [[ "$newrpath" != "$oldrpath" ]]
-              then
-                patchelf --set-rpath "$newrpath" "$path" || echo Error setting rpath: $path
-              fi
-            done
-
-          '' + attrs.preFixup;
-        });
-
-        unityEnv = buildEnv {
-          name = "env-unity";
-          # Custome Unity3d environment.
-          paths = with pin1803; [
-            android-studio
-            android-udev-rules
-            androidndk
-            androidsdk
-          # androidenv.buildTools
-          # androidenv.platformTools
-            fsharp
-            openjdk
-            mono
-            monodevelop
-            steam-run-native
-            unity3d
-          ];
-        };
+        inherit (import ./unity.nix { inherit super pkgs pin1803; }) unity3d unityEnv;
 
         juliaEnv = pkgs.buildEnv {
           name = "env-julia";
---
+++ .config/nixpkgs/unity.nix
@@ -0,0 +1,44 @@
+{
+  super
+, pkgs
+, pin1803
+}:
+
+rec {
+
+        unity3d = super.stdenv.lib.overrideDerivation super.unity3d (attrs: {
+          preFixup = ''
+            find $unitydir -name PlaybackEngines -prune -o \( -type f -name \*.so -not -name libunity-nosuid.so \) -print | while read path
+            do
+              oldrpath=$(patchelf --print-rpath "$path")
+              # TODO: Combine the three sed scripts into one.
+              newrpath=$(echo $oldrpath | sed -e "s/:/\n/g" | sed -e "/^\/usr/d ; /^\/home/d ; /^\/tmp/d" | sed -e :a -e "/$/N; s/\n/:/; ta")
+              if [[ "$newrpath" != "$oldrpath" ]]
+              then
+                patchelf --set-rpath "$newrpath" "$path" || echo Error setting rpath: $path
+              fi
+            done
+
+          '' + attrs.preFixup;
+        });
+
+        unityEnv = pkgs.buildEnv {
+          name = "env-unity";
+          # Custome Unity3d environment.
+          paths = with pin1803; [
+            android-studio
+            android-udev-rules
+            androidndk
+            androidsdk
+          # androidenv.buildTools
+          # androidenv.platformTools
+            fsharp
+            openjdk
+            mono
+            monodevelop
+            steam-run-native
+            unity3d
+          ];
+        };
+
+}
