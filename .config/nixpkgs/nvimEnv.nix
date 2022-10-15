8b747f8273116ec15373fc73d64148b0cda77223
Brian W Bush <code@functionally.io>
2022-10-15 07:17:42 -0600

    Switched to neovim.

--- .config/nixpkgs/nvimEnv.nix
+++ .config/nixpkgs/nvimEnv.nix
@@ -6,43 +6,31 @@ with pkgs;
 let
 
   vimLocal = neovim.override {
+
     viAlias  = true;
     vimAlias = true;
+
     configure = {
       packages.myVimPackage = with pkgs.vimPlugins; {
         start = [
-          airline
-#         deoplete-lsp
           deoplete-nvim
-        # hlint
+          deoplete-vim-lsp
+          hlint-refactor-vim
           LanguageClient-neovim
-#         ncm2
-#         nvim-yarp
-#         nvim-lsp
-        # supertab
+        # nvim-fzf
           tslime
+        # vim-grepper
           vim-hoogle
-        # vim-markdown
-        # vim-nix
-        # vimtex
+        # vim-ormolu
+          vim-lsp
         ];
       };
-      customRC = ''
+
+    customRC = ''
 
       " General settings.
-
-      syntax on
-
-      " Set up filetype.
-
-      filetype indent off
-      filetype plugin on
-
-      " Miscellaneous settings.
-
+      
       set nocompatible
-      "et number
-      "et nowrap
       set showmode
       set tw=10000
       set smartcase
@@ -55,71 +43,59 @@ let
       set incsearch
       set mouse=a
       set history=1000
-      "et clipboard=unnamedplus,autoselect
+
+      set t_Co=256
+      set cmdheight=1
+      set laststatus=2
 
       set completeopt=menuone,menu,longest
-
       set wildignore+=*\\tmp\\*,*.swp,*.swo,*.zip,.git
       set wildmode=longest,list,full
       set wildmenu
-      set completeopt+=longest
-
-      set t_Co=256
-
-      set cmdheight=1
-
-      set laststatus=2
-      set statusline=%<%f\ %h%m%r%=%-14.(%l,%c%V%)\ %P
-    
-     " Turn off autoindent. 
-
-      nnoremap <F8> :setl noai nocin nosi inde=<CR>
- 
-      " Set up supertab.  See <http://www.stephendiehl.com/posts/vim_2016.html#supertab>.
-
-      "et g:SuperTabDefaultCompletionType = '<c-x><c-o>'
-
-      " Set up tslime.  See <https://github.com/jgdavey/tslime.vim>.
-
-      let g:tslime_always_current_session = 1
-      let g:tslime_always_current_window = 1
-
-      vmap gt <Plug>SendSelectionToTmux
-      nmap gt <Plug>NormalModeSendToTmux
-      nmap gv <Plug>SetTmuxVars
-
-      " Set up deoplete.
-      let g:deoplete#enable_at_startup = 1
-
-      " Set up language client.
-
+      
+      " Settings for `LanguageClient-neovim`.
+      
+      set rtp+=~/.vim/pack/XXX/start/LanguageClient-neovim
+      
       let g:LanguageClient_serverCommands = { 'haskell': ['haskell-language-server', '--lsp'] }
+      """"let g:LanguageClient_diagnosticsSignsMax = 0
       let g:LanguageClient_hoverPreview = "Always"
-      "et g:LanguageClient_diagnosticsSignsMax = 0
-
+      
       set omnifunc=LanguageClient#complete
       set completefunc=LanguageClient#complete
       set formatexpr=LanguageClient#textDocument_rangeFormatting_sync()
-
-      map <Leader>ld :call LanguageClient#textDocument_definition()<CR>
-      map <Leader>lh :call LanguageClient#textDocument_hover()<CR>
-      map <Leader>li :call LanguageClient#textDocument_implementation()<CR>
-      map <Leader>lm :call LanguageClient#contextMenu()<CR>
-      map <Leader>lr :call LanguageClient#textDocument_references()<CR>
+      
+      nnoremap <F5> :call LanguageClient_contextMenu()<CR>
+      map <Leader>lk :call LanguageClient#textDocument_hover()<CR>
+      map <Leader>lg :call LanguageClient#textDocument_definition()<CR>
+      map <Leader>lr :call LanguageClient#textDocument_rename()<CR>
+      map <Leader>lf :call LanguageClient#textDocument_formatting()<CR>
+      map <Leader>lb :call LanguageClient#textDocument_references()<CR>
+      map <Leader>la :call LanguageClient#textDocument_codeAction()<CR>
       map <Leader>ls :call LanguageClient#textDocument_documentSymbol()<CR>
-      map <Leader>lt :call LanguageClient#textDocument_typeDefinition()<CR>
-      map <Leader>lu :call LanguageClient#textDocument_documentHighlight()<CR>
-      map <Leader>lw :call LanguageClient#workspace_symbol()<CR>
-      map <Leader>lx :call LanguageClient#clearDocumentHighlight()<CR>
-
+      
       hi link ALEError   Item
       hi link ALEWarning Item
       hi link ALEInfo    Item
-      "i Item cterm=bold ctermfg=LightGray ctermbg=DarkGray
       hi Item cterm=underline ctermfg=Red
+      """"hi Item cterm=bold ctermfg=LightGray ctermbg=DarkGray
+      
+      " Settings for `deoplete-vim-lsp`.
+      
+      let g:deoplete#enable_at_startup = 1
+      
+      " Settings for `tslime-vim`.
+      
+      let g:tslime_always_current_session = 1
+      let g:tslime_always_current_window = 1
+      
+      vmap gt <Plug>SendSelectionToTmux
+      nmap gt <Plug>NormalModeSendToTmux
+      nmap gv <Plug>SetTmuxVars
+     '';
 
-      '';
     };
+
   };
 
 in
